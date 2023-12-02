de7d7af6257ce0cb2ba8d88ad36666e4e7365674
Merajshub <142313878+Merajshub@users.noreply.github.com>
2023-12-02 09:32:46 +0000

    Var types and Data conversion

--- 01_basic/1_var.js
+++ 01_basic/1_var.js
@@ -2,10 +2,22 @@ const accountId = 123;
 let accountEmail = "xyz@gmail.com";
 var accountPass = "12345";
 let accountState;
-
 // var prefer not to use, because of issue in block scope functional scope.
 
-console.log(typeof(accountId))
-console.log(accountId);
 
-console.table([accountId,accountEmail,accountPass,accountState]);
+// console.log(typeof(accountId))
+// console.log(accountId);
+
+// console.table([accountId,accountEmail,accountPass,accountState]);
+
+// Conversion of data type
+
+// let score = "123ab";
+// let score = true;
+let score = "meraj";
+let Score1 = Number(score);
+
+
+
+console.log(Score1);
+console.log(typeof(Score1));
